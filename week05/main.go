94d95e4b94de2e6696bf8fabacaef8c6ca9b1fc7
inhatckimtaeganc <inhatc.kimtaegan.c@gmail.com>
2023-09-25 15:19:45 +0900

    week05 v0.9

--- week05/main.go
+++ week05/main.go
@@ -5,18 +5,28 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	fmt.Print("Input score : ")
 	reader := bufio.NewReader(os.Stdin)
-	inputScore, err := reader.ReadString('\n')
+	inputScoreString, err := reader.ReadString('\n')
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	inputScoreString = strings.TrimSpace(inputScoreString)      // remove space bar
+	inputScore, err := strconv.ParseFloat(inputScoreString, 32) // casting
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	if inputScore >= 90 {
 		grade := "A grade!"
 	} else {
 		grade := "BCDE grade~"
 	}
+	fmt.Println("You got " + grade)
 }
