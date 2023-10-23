d77fd28ca262fabc1d47ec88927f5f43cbbd5b15
inhatckimtaeganc <inhatc.kimtaegan.c@gmail.com>
2023-10-23 14:54:50 +0900

    week09 v1.7

--- week09/main.go
+++ week09/main.go
@@ -2,26 +2,19 @@ package main
 
 import "fmt"
 
-func double(n *int) {
-	*n = *n * 2
+func swap(n1 int, n2 int) {
+	temp := n1
+	n1 = n2
+	n2 = temp
 }
 
 func main() {
-	var a int = 6
-	double(&a)
-	fmt.Println(a)
+	a := 10
+	b := 20 // var b int = 20
+
+	c := &a // var c *int = &a
+	fmt.Printf("%T\n", c)
+	fmt.Println(a, b)
+	swap(a, b)
+	fmt.Println(a, b)
 }
-
-// package main
-
-// import "fmt"
-
-// func double(n int) {
-// 	n = n * 2
-// }
-
-// func main() {
-// 	var a int = 6
-// 	double(a)
-// 	fmt.Println(a)
-// }
