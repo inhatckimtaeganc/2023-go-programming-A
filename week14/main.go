58dd65097240acbaeb00d6b93fb86d6f293445ff
inhatckimtaeganc <inhatc.kimtaegan.c@gmail.com>
2023-11-27 14:22:55 +0900

    week14 v4.6

--- week14/main.go
+++ week14/main.go
@@ -3,16 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	games := make(map[int]string)
-
-	// append
-	games[456] = "성기훈"
-	games[218] = "박해수"
-	games[067] = "강새벽"
-	games[001] = "오일남"
-	games[199] = "알리"
-	games[101] = "아이오아이"
-
+	// map literal
+	games := map[int]string{
+		456: "성기훈",
+		218: "박해수",
+		67:  "강새벽",
+		1:   "오일남",
+		199: "알리",
+		101: "아이오아이",
+	}
+	//fmt.Println(games[100])
 	for _, v := range games {
 		fmt.Println(v)
 	}
